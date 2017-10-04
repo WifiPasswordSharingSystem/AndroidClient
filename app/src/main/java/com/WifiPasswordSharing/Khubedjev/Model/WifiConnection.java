ec076bdf5ffcb38a6128156778c401ba1aa8ebad
Khubedjev <Hubedjev.d.p@gmail.com>
2017-10-04 14:13:34 +0300

    Реализовано подключение к Wifi точкам WPA и WEP

--- app/src/main/java/com/WifiPasswordSharing/Khubedjev/Model/WifiConnection.java
+++ app/src/main/java/com/WifiPasswordSharing/Khubedjev/Model/WifiConnection.java
@@ -1,8 +1,107 @@
 package com.WifiPasswordSharing.Khubedjev.Model;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by stanislavtyrsa on 04.10.17.
  */
-public class WifiConnection {
-    
+public class WifiConnection  extends BroadcastReceiver{
+
+    private WifiManager wifiManager;
+
+    public boolean ConnectToNetworkWPA( String networkSSID, String passwordMD5 )
+    {
+        try {
+            String password = DigestUtils.md5Hex(passwordMD5);
+            WifiConfiguration conf = new WifiConfiguration();
+            conf.SSID = "\"" + networkSSID + "\"";   // Please note the quotes. String should contain SSID in quotes
+
+            conf.preSharedKey = "\"" + password + "\"";
+
+            conf.status = WifiConfiguration.Status.ENABLED;
+            conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.TKIP);
+            conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.CCMP);
+            conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_PSK);
+            conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.TKIP);
+            conf.allowedPairwiseCiphers.set(WifiConfiguration.PairwiseCipher.CCMP);
+
+            Log.d("connecting", conf.SSID + " " + conf.preSharedKey);
+
+            wifiManager.addNetwork(conf);
+
+            Log.d("after connecting", conf.SSID + " " + conf.preSharedKey);
+
+
+
+            List<WifiConfiguration> list = wifiManager.getConfiguredNetworks();
+            for( WifiConfiguration i : list ) {
+                if(i.SSID != null && i.SSID.equals("\"" + networkSSID + "\"")) {
+                    wifiManager.disconnect();
+                    wifiManager.enableNetwork(i.networkId, true);
+                    wifiManager.reconnect();
+                    Log.d("re connecting", i.SSID + " " + conf.preSharedKey);
+                    break;
+                }
+            }
+
+
+            //WiFi Connection success, return true
+            return true;
+        } catch (Exception ex) {
+            System.out.println(Arrays.toString(ex.getStackTrace()));
+            return false;
+        }
+    }
+    public boolean ConnectToNetworkWEP( String networkSSID, String passwordMD5 )
+    {
+        try {
+            String password = DigestUtils.md5Hex(passwordMD5);
+            WifiConfiguration conf = new WifiConfiguration();
+            conf.SSID = "\"" + networkSSID + "\"";   // Please note the quotes. String should contain SSID in quotes
+            conf.wepKeys[0] = "\"" + password + "\""; //Try it with quotes first
+
+            conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+            conf.allowedGroupCiphers.set(WifiConfiguration.AuthAlgorithm.OPEN);
+            conf.allowedGroupCiphers.set(WifiConfiguration.AuthAlgorithm.SHARED);
+
+            int networkId = wifiManager.addNetwork(conf);
+
+            if (networkId == -1){
+                //Try it again with no quotes in case of hex password
+                conf.wepKeys[0] = password;
+                networkId = wifiManager.addNetwork(conf);
+            }
+
+            List<WifiConfiguration> list = wifiManager.getConfiguredNetworks();
+            for( WifiConfiguration i : list ) {
+                if(i.SSID != null && i.SSID.equals("\"" + networkSSID + "\"")) {
+                    wifiManager.disconnect();
+                    wifiManager.enableNetwork(i.networkId, true);
+                    wifiManager.reconnect();
+                    break;
+                }
+            }
+
+            //WiFi Connection success, return true
+            return true;
+        } catch (Exception ex) {
+            System.out.println(Arrays.toString(ex.getStackTrace()));
+            return false;
+        }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+    }
 }
